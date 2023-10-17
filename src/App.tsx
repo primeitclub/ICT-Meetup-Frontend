66f84c3757d9109de81eb64d95651f06bbd7313b
tyzrex <sulavbaral58@gmail.com>
2023-10-17 17:32:52 +0545

    gradient background

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ function App() {
   return (
     <Box
       // bg="radial-gradient(132.21% 68.86% at 43.76% 52.23%, #040E2C 0.47%, rgba(0, 0, 0, 0.80) 100%)"
-      bg="#020618"
+      // bg="#020618"
       // width={"100vw"}
       // height={"100vh"}
+      className="main-body"
     >
       <Box
         maxW={{
