0a6321aeebcbb9cf9ecd9fdc444cea01327daacf
tyzrex <sulavbaral58@gmail.com>
2023-11-17 19:10:10 +0545

    Refactor FAQ and Hero components

--- src/components/Global/Hero.tsx
+++ src/components/Global/Hero.tsx
@@ -3,61 +3,62 @@ import Robot from '../../assets/Robot.png';
 import { Box, Text } from '@chakra-ui/react';
 import { BsArrowRight } from 'react-icons/bs';
 export default function Hero() {
-  return (
-    <>
-      <Box
-        // height={"90vh"}
-        paddingY={{
-          base: "100px",
-          lg: "200px",
-        }}
-        width={"90%"}
-        margin={"auto"}
-        position={"relative"}
-        display={"flex"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        flexDirection={{
-          base: "column",
-          lg: "row",
-        }}
-      >
-        <Flex
-          width={{
-            base: "100%",
-            lg: "50%",
-          }}
-          direction={"column"}
-          gap={"26px"}
-        >
-          <Flex direction={"column"} fontFamily={"heading"}>
-            <Flex justifyContent={"space-between"} width={"70%"}>
-              <Text className="ict-header">ICT</Text>
-              <Text className="version-text">V 7.0</Text>
-            </Flex>
-            <h1 className="meetup-header">MEETUP</h1>
-          </Flex>
-          <Text fontFamily="body" color={"white"} fontSize={"24px"}>
-            Join us for three days of cutting-edge technology, inspiring
-            workshops, exciting competitions and more!
-          </Text>
+ return (
+  <>
+   <Box
+    // height={"90vh"}
+    paddingY={{
+     base: '100px',
+     lg: '200px'
+    }}
+    // width={'90%'}
+    margin={'auto'}
+    position={'relative'}
+    display={'flex'}
+    justifyContent={'space-between'}
+    alignItems={'center'}
+    flexDirection={{
+     base: 'column-reverse',
+     lg: 'row'
+    }}>
+    <Flex
+     width={{
+      base: '100%',
+      lg: '50%'
+     }}
+     direction={'column'}
+     gap={'26px'}>
+     <Flex direction={'column'} fontFamily={'heading'}>
+      <Flex justifyContent={'space-between'} width={'70%'}>
+       <Text className="ict-header">ICT</Text>
+       <Text className="version-text">V 7.0</Text>
+      </Flex>
+      <h1 className="meetup-header">MEETUP</h1>
+     </Flex>
+     <Text color={'white'} fontSize={{ sm: '20px', base: '18px' }}>
+      Join us for three days of cutting-edge technology, inspiring workshops, exciting competitions
+      and more!
+     </Text>
 
      <Button
       variant={'primary-button'}
-      padding={{
-       base: '10px 30px',
-       lg: '10px 50px'
+      padding={'12px 24px'}
+      width={{
+       base: '100%',
+       md: 'fit-content'
       }}
-      width={'fit-content'}
       rounded={'0'}
-      gap={'2'}
-     >
+      gap={'2'}>
       <span>Register Now</span>
       <BsArrowRight />
      </Button>
     </Flex>
 
-    <Flex>
+    <Flex
+     paddingY={{
+      base: '20',
+      lg: '0'
+     }}>
      <img src={Robot} alt="gradient" />
     </Flex>
    </Box>
