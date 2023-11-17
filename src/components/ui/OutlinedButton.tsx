f171fe1586af6ced7983ebf136a9e3641ae8e0b9
tyzrex <sulavbaral58@gmail.com>
2023-11-17 19:11:02 +0545

    Add OutlinedButton component and update Home page layout

---
+++ src/components/ui/OutlinedButton.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@chakra-ui/react';
+
+export default function OutlinedButton({ buttonText }: { buttonText: string }) {
+ return (
+  <>
+   <Button
+    paddingX={'24px'}
+    width={{
+     base: '100%',
+     sm: '321px'
+    }}
+    paddingY={'12px'}
+    variant={'outlined-button'}>
+    {buttonText}
+   </Button>
+  </>
+ );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,46 @@
+import { Container } from '@chakra-ui/react';
 import About from '../../components/Global/About';
 import Events from '../../components/Global/Events';
+import FAQAccordion from '../../components/Global/FaqAccordion';
 import Hero from '../../components/Global/Hero';
+import HomePageGlimpses from '../../components/Global/HomePageGlimpses';
+import SneakPeak from '../../components/Global/SneakPeak';
+import Speakers from '../../components/Global/Speakers';
+import Sponsors from '../../components/Global/Sponsors';
+import JoinUs from '../../components/Global/JoinUs';
 
 const Home = () => {
  return (
   <>
-   <Hero />
-   <About />
-   <Events />
+   <Container
+    maxW={{
+     xl: '1440px'
+    }}
+    paddingX={{
+     base: '32px',
+     xl: '0px'
+    }}
+    margin={'auto'}>
+    <Hero />
+    <About />
+    <Events />
+    <Speakers />
+   </Container>
+   <SneakPeak />
+   <Container
+    maxW={{
+     xl: '1440px'
+    }}
+    paddingX={{
+     base: '32px',
+     xl: '0px'
+    }}
+    margin={'auto'}>
+    <Sponsors />
+    <HomePageGlimpses />
+    <FAQAccordion />
+   </Container>
+   <JoinUs />
   </>
  );
 };
