f1a02117942e7ac514cd64ba1f5b59df4141af5f
Niva02M <nivv2059@gmail.com>
2023-11-20 20:01:32 +0545

    valorant components

--- src/pages/Valorant/valorant.tsx
+++ src/pages/Valorant/valorant.tsx
@@ -1,21 +1,11 @@
 import { Box, Text } from '@chakra-ui/react';
 import ValoBanner from './components/banner';
-import { Tabs, TabList, Tab, TabPanel, TabPanels } from '@chakra-ui/react';
-import { useState } from 'react';
-import MyTeam from './components/myTeam';
-import AllTeams from './components/allTeams';
+import TabSection from './components/TabSection';
 
 function ValorantPage() {
- const [selectedTab, setSelectedTab] = useState(0);
-
- const handleTabChange = (index: number) => {
-  setSelectedTab(index);
- };
-
  return (
   <Box>
    <ValoBanner />
-
    <Box
     sx={{
      width: '100%',
@@ -34,49 +24,7 @@ function ValorantPage() {
      The Teams
      <Text color="var(--Secondary, #E76028)">.</Text>
     </Text>
-
-    <Tabs onChange={handleTabChange} index={selectedTab}>
-     <TabList>
-      <Tab
-       _selected={{
-        bg: 'var(--Primary-Gradient, linear-gradient(92deg, #2C84EC 0.11%, #0055D5 103.68%))'
-       }}
-       display={'flex'}
-       width={{ base: '100%', md: '180px' }}
-       padding={'12px 24px'}
-       justifyContent={'center'}
-       alignItems={'center'}
-       gap={'8px'}
-       color={'white'}
-       border={'1px solid #0055D5'}
-       borderRight={'0'}>
-       All
-      </Tab>
-      <Tab
-       _selected={{
-        bg: 'var(--Primary-Gradient, linear-gradient(92deg, #2C84EC 0.11%, #0055D5 103.68%))'
-       }}
-       display={'flex'}
-       width={{ base: '100%', md: '180px' }}
-       padding={'12px 24px'}
-       justifyContent={'center'}
-       alignItems={'center'}
-       gap={'8px'}
-       color={'white'}
-       border={'1px solid #0055D5'}>
-       My Team
-      </Tab>
-     </TabList>
-
-     <TabPanels>
-      <TabPanel color="white">
-       <AllTeams />
-      </TabPanel>
-      <TabPanel color={'white'}>
-       <MyTeam />
-      </TabPanel>
-     </TabPanels>
-    </Tabs>
+    <TabSection />
    </Box>
   </Box>
  );
