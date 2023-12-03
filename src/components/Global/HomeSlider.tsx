ae036447c496370fd353d45c9b9c5d9fd90f4e9e
tyzrex <sulavbaral58@gmail.com>
2023-12-03 20:06:03 +0545

    Home page redesign

--- src/components/Global/HomeSlider.tsx
+++ src/components/Global/HomeSlider.tsx
@@ -1,50 +1,52 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
-import SpeakerCards from '../reusables/SpeakerCards';
-import SingleImage from '../../assets/susit.png';
-
 import 'swiper/css';
 import 'swiper/css/grid';
 import 'swiper/css/pagination';
 
-export default function HomeSwiper() {
- return (
-  <>
-   <Swiper
-    modules={[Pagination, Navigation]}
-    spaceBetween={50}
-    slidesPerView={3}
-    breakpoints={{
-     0: {
-      slidesPerView: 1,
-      spaceBetween: 20
-     },
-     780: {
-      slidesPerView: 2,
-      spaceBetween: 20
-     },
-     990: {
-      slidesPerView: 3,
-      spaceBetween: 20
-     }
-    }}>
-    <SwiperSlide>
-     <SpeakerCards name="Susit" image={SingleImage} speakerSpeciality="Ui Ux Designer" />
-    </SwiperSlide>
-    <SwiperSlide>
-     <SpeakerCards name="Susit" image={SingleImage} speakerSpeciality="Ui Ux Designer" />
-    </SwiperSlide>
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-    <SwiperSlide>
-     <SpeakerCards name="Susit" image={SingleImage} speakerSpeciality="Ui Ux Designer" />
-    </SwiperSlide>
-    <SwiperSlide>
-     <SpeakerCards name="Susit" image={SingleImage} speakerSpeciality="Ui Ux Designer" />
-    </SwiperSlide>
-    <SwiperSlide>
-     <SpeakerCards name="Susit" image={SingleImage} speakerSpeciality="Ui Ux Designer" />
-    </SwiperSlide>
-   </Swiper>
-  </>
- );
+import SingleImage from '../../assets/susit.png';
+import SpeakerCards from '../reusables/SpeakerCards';
+
+export default function HomeSwiper() {
+  return (
+    <>
+      <Swiper
+        modules={[Pagination, Navigation]}
+        spaceBetween={50}
+        slidesPerView={3}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          780: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          990: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <SpeakerCards name='Susit' image={SingleImage} speakerSpeciality='Ui Ux Designer' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SpeakerCards name='Susit' image={SingleImage} speakerSpeciality='Ui Ux Designer' />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <SpeakerCards name='Susit' image={SingleImage} speakerSpeciality='Ui Ux Designer' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SpeakerCards name='Susit' image={SingleImage} speakerSpeciality='Ui Ux Designer' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SpeakerCards name='Susit' image={SingleImage} speakerSpeciality='Ui Ux Designer' />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
