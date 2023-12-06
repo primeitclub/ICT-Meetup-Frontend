6f00c28ae2ae5345e2af63858fe17e9895eca2b8
tyzrex <sulavbaral58@gmail.com>
2023-12-06 21:05:15 +0545

    Refactor code to improve readability and organization

--- src/pages/AboutUs/Component/AboutUsSection.tsx
+++ src/pages/AboutUs/Component/AboutUsSection.tsx
@@ -1,37 +1,53 @@
-import { Image, Text, Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 
 const AboutUsSection = (props: any) => {
- const { imageLink, heading, paragraph, direction } = props;
- return (
-  <Flex
-   //    key={index}
-   direction={direction === 'left' ? ['column', 'row'] : ['column', 'row-reverse']}
-   mb={'24'}
-   gap={'10'}
-   //    direction={['column', 'row']}
-   justifyContent={'center'}
-   alignItems={'center'}
-   color={'#fff'}>
-   <Box width={['', '50%']}>
-    <Image src={imageLink} alt="Image" />
-   </Box>
-   <Box width={['', '50%']}>
-    <Text
-     variant={'section-header'}
-     fontSize={'32px'}
-     fontWeight={'700'}
-     letterSpacing={'0.64px'}
-     textTransform={'uppercase'}
-     lineHeight={''}
-     fontFamily={'heading'}>
-     {heading}
-    </Text>
-    <Text mt={'24px'} fontSize={'20px'} fontWeight={'500'}>
-     {paragraph}
-    </Text>
-   </Box>
-  </Flex>
- );
+  const { imageLink, heading, paragraph, direction } = props;
+  const flexDirection = direction === "left" ? "row" : "row-reverse";
+  return (
+    <Flex
+      //    key={index}
+      direction={{
+        base: "column",
+        lg: flexDirection,
+      }}
+      mb={"24"}
+      gap={"10"}
+      //    direction={['column', 'row']}
+      justifyContent={"center"}
+      alignItems={"center"}
+      color={"#fff"}
+    >
+      <Box
+        width={{
+          base: "100%",
+          lg: "50%",
+        }}
+      >
+        <Image src={imageLink} alt="Image" />
+      </Box>
+      <Box
+        width={{
+          base: "100%",
+          lg: "50%",
+        }}
+      >
+        <Text
+          variant={"section-header"}
+          fontSize={"32px"}
+          fontWeight={"700"}
+          letterSpacing={"0.64px"}
+          textTransform={"uppercase"}
+          lineHeight={""}
+          fontFamily={"heading"}
+        >
+          {heading}
+        </Text>
+        <Text mt={"24px"} fontSize={"20px"} fontWeight={"500"}>
+          {paragraph}
+        </Text>
+      </Box>
+    </Flex>
+  );
 };
 
 export default AboutUsSection;
--- src/pages/Schedule/Components/TabContent.tsx
+++ src/pages/Schedule/Components/TabContent.tsx
@@ -1,83 +1,104 @@
-import {
- Box,
- Tabs,
- TabList,
- TabPanels,
- Tab,
- Divider,
- shouldForwardProp,
- chakra,
-} from '@chakra-ui/react';
+import { useState } from "react";
 
-import { useState } from 'react';
-import { motion, isValidMotionProp, Transition } from 'framer-motion';
-import jsondata1 from '../../../data/dataSchedule.json';
-import ScheduleData from './ScheduleData ';
+import { isValidMotionProp, motion, Transition } from "framer-motion";
+
+import {
+  Box,
+  chakra,
+  Divider,
+  shouldForwardProp,
+  Tab,
+  TabList,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+
+import jsondata1 from "../../../data/dataSchedule.json";
+import ScheduleData from "./ScheduleData ";
 
 function TabContent() {
- const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(0);
 
+  //  framer motion
 
- //  framer motion
+  const BoxSchedule = chakra(motion.div, {
+    shouldForwardProp: (prop) =>
+      isValidMotionProp(prop) || shouldForwardProp(prop),
+  });
 
- const BoxSchedule = chakra(motion.div, {
-  shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop)
- });
+  const customeTranstion: Transition = {
+    duration: "0.3",
+    ease: "easeOut",
+    type: "spring",
+    stiffness: 200,
+    damping: 15,
+  };
 
- const customeTranstion: Transition = {
-  duration: '0.3',
-  ease: 'easeOut',
-  type: 'spring',
-  stiffness: 200,
-  damping: 15
- };
+  //  store object array
 
- //  store object array
+  return (
+    <>
+      <Tabs onChange={(index) => setActiveTab(index)}>
+        <Box
+          pb={"48px"}
+          className="tab_list_wrpa"
+          width={"fit-content"}
+          margin={"auto"}
+        >
+          <TabList
+            gap={2}
+            fontSize={{ lg: "24px", sm: "20px", base: "20px" }}
+            fontWeight={600}
+            lineHeight={"150%"}
+          >
+            <Tab
+              p={"16px 20px"}
+              border={"2px solid transparent"}
+              _selected={{ border: "2px solid #0055D5", fontWeight: 600 }}
+            >
+              Dec 12
+            </Tab>
 
+            <Tab
+              p={"16px 20px"}
+              border={"2px solid transparent"}
+              _selected={{ border: "2px solid #0055D5", fontWeight: 600 }}
+            >
+              Dec 13
+            </Tab>
+          </TabList>
+        </Box>
 
- return (
-  <>
-   <Tabs onChange={(index) => setActiveTab(index)}>
-    <Box pb={'48px'} className="tab_list_wrpa" width={'fit-content'} margin={'auto'}>
-     <TabList
-      gap={2}
-      fontSize={{ lg: '24px', sm: '20px', base: '20px' }}
-      fontWeight={600}
-      lineHeight={'150%'}>
-      <Tab
-       p={'16px 20px'}
-       border={'2px solid transparent'}
-       _selected={{ border: '2px solid #0055D5',fontWeight:600 }}>
-       Dec 12
-      </Tab>
+        <Divider border={"1px solid #0E1A3B"} />
 
-      <Tab
-       p={'16px 20px'}
-       border={'2px solid transparent'}
-       _selected={{ border: '2px solid #0055D5',fontWeight:600 }}>
-       Dec 13
-      </Tab>
-     </TabList>
-    </Box>
+        <TabPanels
+          className="tab_custome"
+          maxW={{
+            base: "90%",
+            lg: "100%",
+          }}
+          margin={"auto"}
+          overflow={"hidden"}
+        >
+          <BoxSchedule
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            transition={customeTranstion}
+          >
+            <ScheduleData dataScheduleDate={jsondata1.Dec12} />
+          </BoxSchedule>
 
-    <Divider border={'1px solid #0E1A3B'} />
-
-    <TabPanels className='tab_custome' width={{md:"100%",sm:"90%"}} margin={'auto'} overflow={'hidden'} >
-     {/* looopppp */}
-
-     <BoxSchedule initial={{ y: 100 }} animate={{ y: 0 }} transition={customeTranstion}>
-      <ScheduleData dataScheduleDate={jsondata1.Dec12} />
-     </BoxSchedule>
-
-     <BoxSchedule initial={{ y: 100 }} animate={{ y: 0 }} transition={customeTranstion}>
-      <ScheduleData dataScheduleDate={jsondata1.Dec13} />
-     </BoxSchedule>
-
-
-    </TabPanels>
-   </Tabs>
-  </>
- );
+          <BoxSchedule
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            transition={customeTranstion}
+          >
+            <ScheduleData dataScheduleDate={jsondata1.Dec13} />
+          </BoxSchedule>
+        </TabPanels>
+      </Tabs>
+    </>
+  );
 }
 
 export default TabContent;
